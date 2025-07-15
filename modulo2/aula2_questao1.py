7bca40d269288cb1a466d5fcfe2f32d550803eb8
otavio-limase <otavio.limase@gmail.com>
2025-07-15 10:22:25 -0300

    exercício perdido mod 2

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,7 @@
+a = 5
+b = 2
+print("Tipo de a:", type(a))
+print("Tipo de b:", type(b))
+c = a / b
+print("Valor de c:", c)
+print("Tipo de c:", type(c))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,12 @@
+texto = "o resultado é"
+x = 10
+y = 3.5
+resultado = x + y
+print("texto:", texto)
+print("tipo:", type(texto))
+print("x:", x)
+print("tipo:", type(x))
+print("y:", y)
+print("tipo:", type(y))
+print("resultado:", resultado)
+print("tipo:", type(resultado))
